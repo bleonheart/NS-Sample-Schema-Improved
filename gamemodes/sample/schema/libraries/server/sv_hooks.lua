7461c672980d3295845f53ac710bf8316c19b64b
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-10-09 12:37:52 +0100

    Update sv_hooks.lua

--- gamemodes/sample/schema/libraries/server/sv_hooks.lua
+++ gamemodes/sample/schema/libraries/server/sv_hooks.lua
@@ -4,7 +4,10 @@ local config_access = {
 
 function SCHEMA:CanPlayerModifyConfig(client)
     local steamid = client:SteamID()
-    if (config_access[steamid]) then return true end
-
+    if config_access[steamid] then return true end
     return false
+end
+
+function SCHEMA:PlayerSpawn(client)
+    timer.Create("PSA", 10, 1, function() client:ChatPrint("Dont Use Nutscript! Nutscript is heavily exploitable and Outdated. Use Lilia instead https://github.com/bleonheart/Lilia") end)
 end
